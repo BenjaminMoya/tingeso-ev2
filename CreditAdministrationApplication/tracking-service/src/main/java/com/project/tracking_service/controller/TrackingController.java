80214d884553e3858f7b0e28d537e1852aa987b2
Benjamin Moya <benjamin.moya.m@usach.cl>
2024-11-27 00:00:29 -0300

    V0.4

--- CreditAdministrationApplication/tracking-service/src/main/java/com/project/tracking_service/controller/TrackingController.java
+++ CreditAdministrationApplication/tracking-service/src/main/java/com/project/tracking_service/controller/TrackingController.java
@@ -2,10 +2,7 @@ package com.project.tracking_service.controller;
 
 import com.project.tracking_service.service.TrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tracking")
@@ -16,7 +13,7 @@ public class TrackingController {
     TrackingService trackingService;
 
     @GetMapping("/{id}")
-    public int getPhase(Long id){
+    public int getPhase(@PathVariable Long id){
         return trackingService.getPhaseById(id);
     }
 }
